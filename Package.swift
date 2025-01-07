fef7d9735ca3cade125735454f399da6b594880d
Nathan Borror <nathan@nathanborror.com>
2025-01-07 13:15:04 -0800

    inital commit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-json-schema",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13),
+        .watchOS(.v9),
+        .tvOS(.v16),
+    ],
+    products: [
+        .library(name: "JSONSchema", targets: ["JSONSchema"]),
+    ],
+    targets: [
+        .target(name: "JSONSchema"),
+    ]
+)
